762d8096b1b15eaf954d1a80155b51fabe9e7a54
Malte Weiß <malteweiss@yahoo.de>
2019-12-07 23:52:02 +0100

    Initialized project 🎉

---
+++ Assets/Scripts/Editor/SerializationExampleInspector.cs
@@ -0,0 +1,46 @@
+﻿using ObjectSerialization;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SerializationExample))]
+public class SerializationExampleInspector : Editor
+{
+    private SerializationExample _target;
+
+    private void OnEnable()
+    {
+        _target = target as SerializationExample;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        foreach (var obj in _target.Objects)
+        {
+            if (obj.Is<int>())
+            {
+                obj.Value = EditorGUILayout.IntField(obj.GetValue<int>());
+                continue;
+            }
+
+            if (obj.Is<string>())
+            {
+                obj.Value = EditorGUILayout.TextField(obj.GetValue<string>());
+                continue;
+            }
+
+            if (obj.Is<float>())
+            {
+                obj.Value = EditorGUILayout.FloatField(obj.GetValue<float>());
+                continue;
+            }
+
+            if (obj.Is<Color>())
+            {
+                obj.Value = EditorGUILayout.ColorField(obj.GetValue<Color>());
+                continue;
+            }
+
+            // Can be extended for other types.
+        }
+    }
+}
---
+++ Assets/Scripts/SerializationExample.cs
@@ -0,0 +1,9 @@
+﻿using ObjectSerialization;
+using UnityEngine;
+
+public class SerializationExample : MonoBehaviour
+{
+    [SerializeField] private SerializableList _objects = new SerializableList(100, "foo", Color.red);
+
+    public SerializableList Objects => _objects;
+}
